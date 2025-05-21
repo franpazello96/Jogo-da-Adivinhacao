be0b89e0d6222fe91ac093ae5669635567a32c6a
Francielly Pazello <74922124+franpazello96@users.noreply.github.com>
2025-05-21 07:02:41 -0300

    styles input/button/letters/word

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+type InputProps = React.ComponentProps<'input'>
+
+export function Input({ ...rest }: InputProps) {
+  return (
+    <input type="text" className={styles.input} {...rest} />
+  );
+}
